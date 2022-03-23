64391f0385b8dcf849c69a524d8568b71ae9fa05
beingpramudita <pramudita644@gmail.com>
2022-03-23 08:04:39 +0545

    Coding Challenge

---
+++ app/src/main/java/com/example/hello_toast_dma_challenge/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hello_toast_dma_challenge;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount=0;
+    private static final String TAG=MainActivity.class.getSimpleName();
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        if(savedInstanceState !=null){
+            mCount=savedInstanceState.getInt(TAG,0);
+            mShowCount.setText(""+mCount);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(TAG, mCount);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,"Hello Toast", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+
+        }
+    }
+
+}
